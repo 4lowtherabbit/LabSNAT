ce725e731b796641c397b9fc4cda1cf38b135a30
Juntao Zhu <juzhu@microsoft.com>
2019-09-24 18:28:26 +0800

    Add project files.

---
+++ LabSNAT/Controllers/ReproController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LabSNAT.Controllers
+{
+    public class ReproController : Controller
+    {
+        // GET: Repro
+        public string Index(string url)
+        {
+            var request = HttpWebRequest.Create(url);
+            request.GetResponse();
+
+            return "OK";
+        }
+    }
+}
---
+++ LabSNAT/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LabSNAT.MvcApplication" Language="C#" %>
