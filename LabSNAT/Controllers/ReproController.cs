9ce4815d8dc6c4251f349c6d1bc407652b6a09e7
Juntao Zhu <juzhu@microsoft.com>
2019-09-28 19:56:48 +0800

    add httpclient support in ReproController

--- LabSNAT/Controllers/ReproController.cs
+++ LabSNAT/Controllers/ReproController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -15,6 +17,34 @@ namespace LabSNAT.Controllers
             var request = HttpWebRequest.Create(url);
             request.GetResponse();
 
+            return "OK";
+        }
+
+        public string Fin(string url)
+        {
+            var request = HttpWebRequest.Create(url);
+            var response = request.GetResponse();
+            response.Close();
+
+            return "OK";
+        }
+
+        public async Task<string> Client(string url)
+        {
+            using (var client = new HttpClient())
+            {
+                await client.GetAsync(url);
+            }
+
+            return "OK";
+        }
+
+        private static Lazy<HttpClient> _client = new Lazy<HttpClient>();
+
+        public async Task<string> ReuseClient(string url)
+        {
+            var client = _client.Value;
+            await client.GetAsync(url);
             return "OK";
         }
     }
